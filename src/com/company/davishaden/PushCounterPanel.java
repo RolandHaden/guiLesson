398fcff608ac6b8ab0fd3e30ea0cfd0fafa0930c
Roland Haden <davis.haden@gmail.com>
2021-09-16 09:46:36 -0400

    added a new button to close the program

--- src/com/company/davishaden/PushCounterPanel.java
+++ src/com/company/davishaden/PushCounterPanel.java
@@ -8,9 +8,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
+
 public class PushCounterPanel extends JPanel {
     private int count;
-    private JButton push,pushReduce;
+    private JButton push,pushReduce,closeWindow;
     private JLabel label;
     private JPanel buttonPanel;
     private JPanel mainPanel;
@@ -25,16 +26,19 @@ public class PushCounterPanel extends JPanel {
         push.addActionListener(new ButtonListener());
         pushReduce = new JButton("Reduce!");
         pushReduce.addActionListener(new ButtonListener());
+        closeWindow = new JButton("Close the Application!");
+        closeWindow.addActionListener(new ButtonListener());
         label = new JLabel("Number: " + count);
         add(label);
         buttonPanel = new JPanel();
         buttonPanel.setPreferredSize(new Dimension(300, 100));
         buttonPanel.add(push);
         buttonPanel.add(pushReduce);
+        buttonPanel.add(closeWindow);
         buttonPanel.setBackground(Color.black);
         add(buttonPanel);
         setBackground(Color.white);
-        setPreferredSize(new Dimension(300, 60));
+        setPreferredSize(new Dimension(300, 120));
     }
     private class ButtonListener implements ActionListener
     {
@@ -50,6 +54,9 @@ public class PushCounterPanel extends JPanel {
                 count--;
                 setBackground(new Color((int) (Math.random() * 100), (int) (Math.random() * 100), (int) (Math.random() * 100)));
                 label.setText("Number: " + count);
+            }
+            if(event.getSource() == closeWindow){
+                System.exit(0); //Closing the program
             }
         }
     }
